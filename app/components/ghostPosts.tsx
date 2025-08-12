dfd72da6cd13429af670bc0a87bc805505f1a097
Joe Fleming <joe@joefleming.co.uk>
2025-08-12 20:57:59 +0100

    add ghost posts

---
+++ app/components/ghostPosts.tsx
@@ -0,0 +1,14 @@
+import styles from "./ghostPosts.module.css";
+
+export default function GhostPosts() {
+  return (
+    <ul className={styles.ghostPosts}>
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+      <li />
+    </ul>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Suspense } from "react";
 
-export default async function Home() {
+async function BlogPosts() {
   let res = await fetch("https://api.vercel.app/blog");
   let posts = await res.json();
+
+  return (
+    <ul className={styles.posts}>
+      {posts.map((post: any) => (
+        <li key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default async function Home() {
   return (
     <div className={styles.page}>
       <h1>Posts</h1>
-      <ul className={styles.posts}>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.description}</p>
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback={<p>Loading...</p>}>
+        <BlogPosts />
+      </Suspense>
     </div>
   );
 }
