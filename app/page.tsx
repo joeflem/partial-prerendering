4d683b29af946b8d2af85b26a64fea913ecafcb8
Joe Fleming <joe@joefleming.co.uk>
2025-08-12 21:13:23 +0100

    upgrade to nextjs canary

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Suspense } from "react";
+import GhostPosts from "./components/ghostPosts";
 
 async function BlogPosts() {
   let res = await fetch("https://api.vercel.app/blog");
   let posts = await res.json();
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
   return (
     <ul className={styles.posts}>
@@ -21,8 +24,18 @@ async function BlogPosts() {
 export default async function Home() {
   return (
     <div className={styles.page}>
+      <h1>Welcome</h1>
+      <Image
+        src="https://melvinnicholsonphotography.co.uk/wp-content/uploads/2019/04/0C5A4247-1-scaled.jpg"
+        alt="Lake District"
+        width={500}
+        height={300}
+      />
+      <p>This is a sample application to demonstrate Next.js features.</p>
+      <p>Check out the blog posts below:</p>
       <h1>Posts</h1>
-      <Suspense fallback={<p>Loading...</p>}>
+      <p></p>
+      <Suspense fallback={<GhostPosts />}>
         <BlogPosts />
       </Suspense>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://melvinnicholsonphotography.co.uk/wp-content/uploads/2019/04/0C5A4247-1-scaled.jpg"
+      ),
+    ],
+  },
+  experimental: {
+    ppr: "incremental",
+  },
 };
 
 export default nextConfig;
